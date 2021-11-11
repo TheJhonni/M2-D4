8d8814ab7fe94ad95d3cc2a9ea98b9932a46b258
TheJhonni <davideconi91@gmail.com>
2021-11-11 15:58:17 +0100

    updated functions name + random group

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const listNames = ["name1", "name2", "name3", "name4", "name5", "name6", "name7", "name8", "name9", "name10", "name11", "name12", "name13","name14","name15"];
 
-let numberTeams = listNames.length / 5;
+let waitingList = listNames[3, 5, 8, 9, 10];
+
+let numberTeams = [1,2,3];
+
+let assignedTeam = 0;
 
 const displayNames = function() {
-    let NamesContainer = document.querySelector("container-fluid")
-    for(let i=0; i<listNames.length; i++) {
-        const names = listNames[i];
 
-        NamesContainer.innerHTML += `
-        <ul class="list-unstyled card-columns">
+    let NamesContainer = document.getElementsByClassName(".container-fluid")
+    for(let i=0; i<listNames.length; i++) {
+
+        for(lety=0; y<numberTeams.length; y++) {
+            let randomTeam = Math.floor(Math.random) * numberTeams.length;
+            
+            NamesContainer.innerHTML += `
+            <ul class="list-unstyled card-columns">
             <ul class="row">
-                <li class="col">${names[i]}</li>
-                <li class="col">Group</li>
+            <li class="col">${listNames[i]}</li>
+            <li class="col">${randomTeam[y]}</li>
             </ul>
-        </ul>` ;
+            </ul>` ;
+        } 
     }
 };
 
-window.onload = function () {
+
+window.onload = function (){
     displayNames()
 }
